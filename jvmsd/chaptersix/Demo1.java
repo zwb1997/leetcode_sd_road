e3b5f6ab8efca5d7a18a94eea073fa342520341a
zwb1997 <IIIIINNNCCC@outlook.com>
2021-05-07 15:59:26 +0800

    new files

---
+++ jvmsd/chaptersix/Demo1.java
@@ -0,0 +1,16 @@
+package jvmsd.chaptersix;
+
+public class Demo1 {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(0X11111111);
+        System.out.println(Math.pow(2.0, 8));
+        System.out.println(2 ^ 3);
+    }
+}
--- linkedlist/Demo1.java
+++ linkedlist/Demo1.java
@@ -5,14 +5,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Demo1 {
-    
-
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(null);
-        System.out.println(list.indexOf(null));
+        System.out.println(0 ^ 3 ^ 3);
     }
 }
