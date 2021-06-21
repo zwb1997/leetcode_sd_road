85797ca6f739978f4cc1b044489440848f092b75
zwb1997 <IIIIINNNCCC@outlook.com>
2021-06-21 22:01:01 +0800

    put some codes

---
+++ jvmsd/chapterseven/common/NotInitialization.java
@@ -0,0 +1,8 @@
+package jvmsd.chapterseven.common;
+
+public class NotInitialization {
+    public static void main(String[] args) {
+        System.out.println(ConstantClass.HELLOWORLD);
+        System.out.println(ConstantClass.i1);
+    }
+}
